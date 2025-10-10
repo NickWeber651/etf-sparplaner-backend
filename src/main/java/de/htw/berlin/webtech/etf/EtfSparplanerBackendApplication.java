e3812c6cdcb0f4aac85506583d11834e480bff7a
NickWeber651 <N.Weber@Student.HTW-Berlin.de>
2025-10-10 16:27:16 +0200

    Flatten: move Gradle project to repo root

---
+++ src/main/java/de/htw/berlin/webtech/etf/EtfSparplanerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.webtech.etf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtfSparplanerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtfSparplanerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/htw/berlin/webtech/etf/EtfSparplanerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.webtech.etf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfSparplanerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
